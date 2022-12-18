748358b754dc68d4179d50923f24899568a27673
AlmondChips <chips6t@gmail.com>
2022-12-18 18:20:46 +0300

    feat: add settings section for products list

--- src/app/components/ProductsPage/ProductsPage.ts
+++ src/app/components/ProductsPage/ProductsPage.ts
@@ -8,15 +8,62 @@ export class ProductsPage extends Component {
 
   productsContainer = new Component(this.node, 'div', 'products-container');
 
-  constructor(parentNode: HTMLElement | null) {
-    super(parentNode, 'div', 'products-page');
+  private productsFound: Component;
 
-    products.forEach((el) => {
-      const card = new Card(null);
-      card.productName.innerText = el.name;
-      card.productSize.node.innerText = el.size;
-      card.productShape.node.innerText = el.shape;
-      this.productsContainer.node.append(card.node);
-    });
+  constructor(parentNode: HTMLElement | null) {
+    super(parentNode, 'div', 'products-page wrapper');
+
+    // products.forEach((el) => {
+    //   const card = new Card(null);
+    //   card.productName.innerText = el.name;
+    //   card.productSize.node.innerText = el.size;
+    //   card.productShape.node.innerText = el.shape;
+    //   this.productsContainer.node.append(card.node);
+    // });
+
+    // two main blocks in product-container
+    const listSettings = new Component(this.productsContainer.node, 'article', 'list-settings');
+    const productList = new Component(this.productsContainer.node, 'div', 'products-list');
+
+    // settings list components
+    const sortOptions = new Component(listSettings.node, 'select', 'sort-options');
+    const sortLable = new Component(sortOptions.node, 'option', 'sort-lable', 'Sort options:')
+      .node as HTMLOptionElement;
+
+    sortLable.selected = true;
+    sortLable.disabled = true;
+
+    (
+      new Component(sortOptions.node, 'option', 'sort-item', 'Sort by price ASC')
+        .node as HTMLOptionElement
+    ).value = 'price-ASC';
+    (
+      new Component(sortOptions.node, 'option', 'sort-item', 'Sort by price DESC')
+        .node as HTMLOptionElement
+    ).value = 'price-DESC';
+
+    (
+      new Component(sortOptions.node, 'option', 'sort-item', 'Sort by rating ASC')
+        .node as HTMLOptionElement
+    ).value = 'rating-ASC';
+    (
+      new Component(sortOptions.node, 'option', 'sort-item', 'Sort by rating DESC')
+        .node as HTMLOptionElement
+    ).value = 'rating-DESC';
+
+    // product found lable
+    const productFoundLable = new Component(listSettings.node, 'p', 'product-found-lable');
+    this.productsFound = new Component(productFoundLable.node, 'span', 'product-found-qty', '101');
+    new Component(productFoundLable.node, 'span', 'product-found-text', ' Product Found');
+
+    // decorative line - hr
+    new Component(listSettings.node, 'div', 'deco-line');
+
+    // items display buttons
+    const buttonsContainer = new Component(listSettings.node, 'div', 'btns-container');
+    const btnGridDisplay = new Component(buttonsContainer.node, 'button', 'btn btn-grid-display');
+    const btnRowDisplay = new Component(buttonsContainer.node, 'button', 'btn btn-row-display');
+    new Component(btnGridDisplay.node, 'img', 'grid-icon');
+    new Component(btnRowDisplay.node, 'img', 'row-icon');
   }
 }
