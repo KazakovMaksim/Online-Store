908fab0eaa924cc959a3326ce21e2bd5b2092852
AlmondChips <chips6t@gmail.com>
2022-12-24 00:28:14 +0300

    feat: perhaps critical changes in hash reading for correct navigation to details page

---
+++ src/app/core/app.ts
@@ -0,0 +1,51 @@
+import { Component } from '../components/component';
+import { Footer } from '../components/Footer/Footer';
+import { Header } from '../components/Header/Header';
+import { HomePage } from '../components/HomePage/HomePage';
+import { CartPage } from '../components/CartPage/CartPage';
+import { ProductsPage } from '../components/ProductsPage/ProductsPage';
+import { routing, defaultRoute } from './router';
+import { ProductDetailsPage } from '../components/ProductDetailsPage/productDetails';
+
+export class App {
+  main = new Component(null, 'main', 'main');
+
+  mainContent: null | CartPage | HomePage | ProductsPage = null;
+
+  enableRouteChange = (): void => {
+    window.onpopstate = () => {
+      const hash = window.location.hash;
+      const currentRouteName = hash.slice(
+        1,
+        hash.indexOf('/') === -1 ? undefined : hash.indexOf('/'),
+      );
+      const currentRoute = routing.find((p) => p.name === currentRouteName);
+      this.mainContent?.node.remove();
+
+      if (!document.body.children.length) {
+        document.body.append(new Header().node, this.main.node, new Footer().node);
+      }
+
+      if (currentRouteName === 'products') {
+        this.mainContent = currentRoute?.component() as ProductsPage;
+      }
+      if (currentRouteName === 'cart') {
+        this.mainContent = currentRoute?.component() as CartPage;
+      }
+      if (currentRouteName === 'product-details') {
+        const productId = Number(hash.slice(hash.indexOf('/') + 1));
+        this.mainContent = currentRoute?.component(productId) as ProductDetailsPage;
+      }
+
+      if (currentRoute && this.mainContent) {
+        this.main.node.append(this.mainContent.node);
+      } else {
+        this.mainContent = defaultRoute.component() as HomePage;
+        this.main.node.append(this.mainContent.node);
+      }
+    };
+
+    const popstateEvent = new Event('popstate');
+    window.dispatchEvent(popstateEvent);
+  };
+}
