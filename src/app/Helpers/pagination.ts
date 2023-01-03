5e48f5b350a4b1362d2123126580bdd72af7cbd8
AlmondChips <chips6t@gmail.com>
2023-01-03 21:38:54 +0300

    feat: implement cart page pagination

--- src/app/Helpers/pagination.ts
+++ src/app/Helpers/pagination.ts
@@ -1,9 +1,23 @@
-import { CartController } from './cartController';
-
 export class Pagination {
-  static setLimit(limit: number) {
-    const url = new URL(window.location.hash);
+  static setLimit(limit: number): void {
+    const url = new URL(window.location.href);
     url.searchParams.set('limit', limit.toString());
-    window.location.href = url.href.toString();
+    history.pushState(null, '', url);
+  }
+
+  static getLimit(): string | null {
+    const url = new URL(window.location.href);
+    return url.searchParams.get('limit');
+  }
+
+  static setPage(page: number): void {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', page.toString());
+    history.pushState(null, '', url);
+  }
+
+  static getPage(): number | null {
+    const url = new URL(window.location.href);
+    return Number(url.searchParams.get('page'));
   }
 }
--- src/app/components/CartPage/CartPage.ts
+++ src/app/components/CartPage/CartPage.ts
@@ -6,6 +6,7 @@ import './CartPage.scss';
 
 export class CartPage extends Component {
   updateCartItemsList: () => void;
+  maxPages = 1;
 
   constructor(parentNode: HTMLElement | null) {
     super(parentNode, 'div', 'cart-page wrapper');
@@ -29,22 +30,34 @@ export class CartPage extends Component {
     const pageNumber = new Component(pageControlsBlock.node, 'span', 'page-number', '1');
     const pageUp = new Component(pageControlsBlock.node, 'button', 'btn btn-page-up', '>');
 
-    pageDown.node.addEventListener('click', () => {
-      const currentNum = Number(pageNumber.node.textContent);
-      if (currentNum > 1) pageNumber.node.textContent = String(currentNum - 1);
-      Pagination.setLimit(1);
-    });
-
-    pageUp.node.addEventListener('click', () => {
-      const currentNum = Number(pageNumber.node.textContent);
-      if (currentNum < 5) pageNumber.node.textContent = String(currentNum + 1);
-    });
-
     const cartItemsList = new Component(cartContainer.node, 'div', 'cart-page-items-list');
 
+    // functions & event-handlers
+    const setMaxPages = () => {
+      const itemsQty = CartController.getCartItems()?.length;
+      const limit = Number(Pagination.getLimit());
+      if (itemsQty && limit) {
+        let division = itemsQty / limit;
+        if (!Number.isInteger(division)) division = Math.floor(division) + 1;
+        this.maxPages = division;
+        const currentPage = Pagination.getPage();
+        if (currentPage) if (this.maxPages < currentPage) updatePageNumber(this.maxPages);
+      }
+    };
+
+    const updateLimit = (value?: number) => {
+      (limitInput.node as HTMLInputElement).value = Pagination.getLimit() || '3';
+      if (value) Pagination.setLimit(value);
+      setMaxPages();
+    };
+
+    updateLimit();
     this.updateCartItemsList = () => {
       const CartItems = CartController.getCartItems();
-      if (CartItems?.length === 0) {
+      const cartLength = CartItems?.length;
+      const limit = Number(Pagination.getLimit());
+      const page = Pagination.getPage();
+      if (cartLength === 0) {
         const emptyMsg = new Component(
           cartItemsList.node,
           'span',
@@ -53,14 +66,54 @@ export class CartPage extends Component {
         );
         const linkToProducts = new Component(emptyMsg.node, 'a', 'product-link', ' Fill It Now!');
         (linkToProducts.node as HTMLAnchorElement).href = '#products';
-      } else {
+      } else if (limit && page) {
         cartItemsList.node.textContent = '';
-        CartItems?.forEach((item) => {
-          new CartItem(cartItemsList.node, item.id, this);
+        CartItems?.forEach((item, index) => {
+          if (index + 1 > (page - 1) * limit && index + 1 < page * limit + 1)
+            new CartItem(cartItemsList.node, item.id, this);
         });
       }
     };
 
     this.updateCartItemsList();
+
+    const updatePageNumber = (num: number) => {
+      pageNumber.node.textContent = String(num);
+      Pagination.setPage(num);
+      this.updateCartItemsList();
+    };
+
+    pageDown.node.addEventListener('click', () => {
+      const currentNum = Number(pageNumber.node.textContent);
+      if (currentNum > 1) {
+        const newNum = currentNum - 1;
+        updatePageNumber(newNum);
+      }
+    });
+
+    pageUp.node.addEventListener('click', () => {
+      const currentNum = Number(pageNumber.node.textContent);
+      if (currentNum < this.maxPages) {
+        const newNum = currentNum + 1;
+        updatePageNumber(newNum);
+      }
+    });
+
+    limitInput.node.addEventListener('input', () => {
+      const limitInputValue = Number((limitInput.node as HTMLInputElement).value);
+      const cartItems = CartController.getCartItems();
+      if (limitInputValue < 1) {
+        updateLimit(1);
+        return;
+      }
+      if (cartItems)
+        if (cartItems?.length < limitInputValue) {
+          updateLimit(cartItems?.length);
+          return;
+        }
+      Pagination.setLimit(limitInputValue);
+      setMaxPages();
+      this.updateCartItemsList();
+    });
   }
 }
