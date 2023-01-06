afc2470bb16cfd3670d0f0544099349c293be72c
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-01-06 22:06:28 +0400

    feat: add name and address validity check

--- src/app/components/Modal/Modal.ts
+++ src/app/components/Modal/Modal.ts
@@ -3,6 +3,11 @@ import './Modal.scss';
 
 export class Modal extends Component {
   formInfo: Component;
+  expire: Component | null = null;
+  name: Component;
+  phone: Component;
+  address: Component;
+  mail: Component;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'modal');
@@ -11,11 +16,21 @@ export class Modal extends Component {
     new Component(formContainer.node, 'h3', 'form-title', 'personal data');
     this.formInfo = new Component(formContainer.node, 'form', 'form-info');
 
-    ['name', 'phone number', 'delivery address', 'e-mail'].forEach((elem) => {
+    [this.name, this.phone, this.address, this.mail] = [
+      'name',
+      'phone number',
+      'delivery address',
+      'e-mail',
+    ].map((elem) => {
       const inputBox = new Component(this.formInfo.node, 'div', 'input-box');
       const input = new Component(inputBox.node, 'input', 'input');
       input.node.setAttribute('placeholder', elem);
+      return input;
     });
+    this.name.node.setAttribute(
+      'title',
+      'field must have at least two words with min length 3 letters',
+    );
     new Component(formContainer.node, 'h3', 'form-title', 'credit card details');
 
     const cardContainer = new Component(formContainer.node, 'div', 'card-container');
@@ -48,19 +63,69 @@ export class Modal extends Component {
       const label = new Component(cardInputs.node, 'label', 'card-label', elem);
       label.node.setAttribute('for', elem);
       const input = new Component(cardInputs.node, 'input');
-      const max = elem === 'Cvc' ? '3' : '4';
+      const max = elem === 'Cvc' ? '3' : '5';
       input.node.setAttribute('maxlength', max);
       input.node.setAttribute('id', elem);
+      if (elem === 'Expire') this.expire = input;
+
       input.node.oninput = () => {
         (input.node as HTMLInputElement).value = (input.node as HTMLInputElement).value.replace(
           /[^\d]/g,
           '',
         );
       };
+      (this.expire?.node as HTMLInputElement).oninput = () => {
+        (this.expire?.node as HTMLInputElement).value = (
+          this.expire?.node as HTMLInputElement
+        ).value.replace(/[^[/]\d]/g, '');
+        if ((this.expire?.node as HTMLInputElement).value.length === 2) {
+          (this.expire?.node as HTMLInputElement).value =
+            (this.expire?.node as HTMLInputElement).value + '/';
+        }
+      };
     });
     const confirmBtn = new Component(formContainer.node, 'button', 'form-btn', 'confirm');
+
     confirmBtn.node.onclick = () => {
+      const nameVal = (this.name.node as HTMLInputElement).value;
+      console.log('checkNameInp=', this.checkNameOrAddressInp(nameVal, 'name'));
+      const addressVal = (this.address.node as HTMLInputElement).value;
+      console.log('checkAddressInp=', this.checkNameOrAddressInp(addressVal, 'address'));
+
       this.node.classList.remove('modal-active');
+      (this.formInfo.node as HTMLFormElement).reset();
     };
+    this.node.onclick = (e) => {
+      if (e.target === this.node) this.node.classList.remove('modal-active');
+    };
+  }
+
+  checkWordsNum(str: string, minWordsNum: number) {
+    return str.trim().split(' ').length >= minWordsNum;
+  }
+
+  checkWordsValidity(str: string, minWordLength: number) {
+    const words = str.trim().split(' ');
+    const regExp = minWordLength === 5 ? /^\w{5}/ : /^\w{3}/;
+    const validWords = words.filter((word) => regExp.test(word));
+    return words.length === validWords.length;
+  }
+
+  checkNameOrAddressInp(inputStr: string, goal: string) {
+    const [numLimit, lengthLimit] = goal === 'name' ? [2, 3] : [3, 5];
+    if (
+      !this.checkWordsNum(inputStr, numLimit) ||
+      !this.checkWordsValidity(inputStr, lengthLimit)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  checkNumberValidity(str: string, minWordLength: number) {
+    const words = str.trim().split(' ');
+    const regExp = minWordLength === 5 ? /^\w{5}/ : /^\w{3}/;
+    const validWords = words.filter((word) => regExp.test(word));
+    return words.length === validWords.length;
   }
 }
